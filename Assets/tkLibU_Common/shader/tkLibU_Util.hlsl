234a2913e0a4a0aad0c79099f307108655250b2c
KawaharaKiyohara <nezumimusume@gmail.com>
2022-05-11 23:02:00 +0900

    iOSでの表示バグを修正。

--- Assets/tkLibU_Common/shader/tkLibU_Util.hlsl
+++ Assets/tkLibU_Common/shader/tkLibU_Util.hlsl
@@ -6,11 +6,9 @@
 inline float2 CalcUVCoordFromClip(float4 coordInClipSpace )
 {
     float2 uv = coordInClipSpace.xy / coordInClipSpace.w;
-#if defined(SHADER_API_D3D11) || defined(SHADER_API_GLCORE) || defined(SHADER_API_D3D11_9X) || defined(TK_DEFERRED_PASS)
-    uv *= float2(0.5f, -0.5f);
-#else
-    uv *= float2(0.5f, 0.5f);
-#endif
+
+    uv *= float2(0.5f, 0.5f * _ProjectionParams.x);
+
     uv += 0.5f;
 
     return uv;
